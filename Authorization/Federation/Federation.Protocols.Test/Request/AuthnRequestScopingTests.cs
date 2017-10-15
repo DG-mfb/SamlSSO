418fb66cb487f3ff6f1b05c421550c1c2f9f0f8f
dg-mfb <danail.georgiev73@gmail.com>
2017-10-15 17:09:58 +0100

    one more scoping tests

--- Authorization/Federation/Federation.Protocols.Test/Request/AuthnRequestScopingTests.cs
+++ Authorization/Federation/Federation.Protocols.Test/Request/AuthnRequestScopingTests.cs
@@ -42,7 +42,7 @@ namespace Federation.Protocols.Test.Request
             var requestUri = new Uri("http://localhost:59611/");
             var federationPartyContextBuilder = new FederationPartyContextBuilderMock();
             var scopingConfiguration = new ScopingConfiguration("http://localhost:59611/") { PoxyCount = 10 };
-            var federationContext = federationPartyContextBuilder.BuildContext("local",scopingConfiguration);
+            var federationContext = federationPartyContextBuilder.BuildContext("local", scopingConfiguration);
             var supportedNameIdentifierFormats = new List<Uri> { new Uri(NameIdentifierFormats.Transient) };
             var authnRequestContext = new AuthnRequestContext(requestUri, federationContext, supportedNameIdentifierFormats);
             var requestConfiguration = federationContext.GetRequestConfigurationFromContext();
@@ -57,6 +57,31 @@ namespace Federation.Protocols.Test.Request
             Assert.AreEqual("10", authnRequest.Scoping.ProxyCount);
             Assert.AreEqual(1, authnRequest.Scoping.RequesterId.Length);
             Assert.AreEqual("http://localhost:59611/", authnRequest.Scoping.RequesterId[0]);
+        }
+
+        [Test]
+        public void BuildAuthnRequest_test_scoping_default_overwritten_2_requesters()
+        {
+            //ARRANGE
+            var requestUri = new Uri("http://localhost:59611/");
+            var federationPartyContextBuilder = new FederationPartyContextBuilderMock();
+            var scopingConfiguration = new ScopingConfiguration("http://localhost:59611/", "http://localhost:59612/") { PoxyCount = 10 };
+            var federationContext = federationPartyContextBuilder.BuildContext("local", scopingConfiguration);
+            var supportedNameIdentifierFormats = new List<Uri> { new Uri(NameIdentifierFormats.Transient) };
+            var authnRequestContext = new AuthnRequestContext(requestUri, federationContext, supportedNameIdentifierFormats);
+            var requestConfiguration = federationContext.GetRequestConfigurationFromContext();
+            AuthnRequestHelper.GetBuilders = AuthnRequestBuildersFactoryMock.GetBuildersFactory();
+
+            //ACT
+            var authnRequest = AuthnRequestHelper.BuildAuthnRequest(authnRequestContext);
+
+            //ASSERT
+            Assert.NotNull(authnRequest);
+            Assert.IsNotNull(authnRequest.Scoping);
+            Assert.AreEqual("10", authnRequest.Scoping.ProxyCount);
+            Assert.AreEqual(2, authnRequest.Scoping.RequesterId.Length);
+            Assert.AreEqual("http://localhost:59611/", authnRequest.Scoping.RequesterId[0]);
+            Assert.AreEqual("http://localhost:59612/", authnRequest.Scoping.RequesterId[1]);
         }
     }
 }
