cc9431f5f0890336ba17afdcc8566d1d0c9e8aa9
dg-mfb <danail.georgiev73@gmail.com>
2017-10-22 17:51:55 +0100

    clean up

--- Authorization/Federation/Federation.Metadata.Consumer.Tests/MetadataHandlerFactoryTests.cs
+++ Authorization/Federation/Federation.Metadata.Consumer.Tests/MetadataHandlerFactoryTests.cs
@@ -15,29 +15,31 @@ namespace Federation.Metadata.Consumer.Tests
         [Test]
         public void GetDelegateForIdpDescriptors_entity_descriptor_metadata_Test()
         {
-            //ARRANGE
-            var metadata = EntityDescriptorProviderMock.GetEntityDescriptor();
-            var handler = new MetadataEntitityDescriptorHandler();
-            //ACT
-            var del = IdpMetadataHandlerFactory.GetDelegateForIdpDescriptors(typeof(EntityDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
-            var idps = del(handler, metadata)
-                .ToList();
-            //ASSERT
-            Assert.AreEqual(1, idps.Count);
+            throw new NotImplementedException();
+            ////ARRANGE
+            //var metadata = EntityDescriptorProviderMock.GetEntityDescriptor();
+            //var handler = new MetadataEntitityDescriptorHandler();
+            ////ACT
+            //var del = IdpMetadataHandlerFactory.GetDelegateForIdpDescriptors(typeof(EntityDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
+            //var idps = del(handler, metadata)
+            //    .ToList();
+            ////ASSERT
+            //Assert.AreEqual(1, idps.Count);
         }
 
         [Test]
         public void GetDelegateForIdpDescriptors_entities_descriptor_metadata_Test()
         {
-            //ARRANGE
-            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor();
-            var handler = new MetadataEntitiesDescriptorHandler();
-            //ACT
-            var del = IdpMetadataHandlerFactory.GetDelegateForIdpDescriptors(typeof(EntitiesDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
-            var idps = del(handler, metadata)
-                .ToList();
-            //ASSERT
-            Assert.AreEqual(1, idps.Count);
+            throw new NotImplementedException();
+            ////ARRANGE
+            //var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor();
+            //var handler = new MetadataEntitiesDescriptorHandler();
+            ////ACT
+            //var del = IdpMetadataHandlerFactory.GetDelegateForIdpDescriptors(typeof(EntitiesDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
+            //var idps = del(handler, metadata)
+            //    .ToList();
+            ////ASSERT
+            //Assert.AreEqual(1, idps.Count);
         }
 
         #region metadata Path tests
@@ -45,29 +47,31 @@ namespace Federation.Metadata.Consumer.Tests
         [Test]
         public void GetDelegateForIdpLocation_entity_descriptor_metadata_Test()
         {
+            throw new NotImplementedException();
             //ARRANGE
-            var expected = new Uri("http://localhost:60879");
-            var metadata = EntityDescriptorProviderMock.GetEntityDescriptor();
-            var handler = new MetadataEntitityDescriptorHandler();
-            //ACT
-            var del = IdpMetadataHandlerFactory.GetDelegateForIdpLocation(typeof(EntityDescriptor));
-            var uri = del(handler, metadata, new Uri(ProtocolBindings.HttpRedirect));
-            //ASSERT
-            Assert.AreEqual(expected, uri);
+            //var expected = new Uri("http://localhost:60879");
+            //var metadata = EntityDescriptorProviderMock.GetEntityDescriptor();
+            //var handler = new MetadataEntitityDescriptorHandler();
+            ////ACT
+            //var del = IdpMetadataHandlerFactory.GetDelegateForIdpLocation(typeof(EntityDescriptor));
+            //var uri = del(handler, metadata, new Uri(ProtocolBindings.HttpRedirect));
+            ////ASSERT
+            //Assert.AreEqual(expected, uri);
         }
 
         [Test]
         public void GetDelegateForIdpLocation_entities_descriptor_metadata_Test()
         {
-            //ARRANGE
-            var expected = new Uri("http://localhost:60879");
-            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor();
-            var handler = new MetadataEntitiesDescriptorHandler();
-            //ACT
-            var del = IdpMetadataHandlerFactory.GetDelegateForIdpLocation(typeof(EntitiesDescriptor));
-            var uri = del(handler, metadata, new Uri(ProtocolBindings.HttpRedirect));
-            //ASSERT
-            Assert.AreEqual(expected, uri);
+            throw new NotImplementedException();
+            ////ARRANGE
+            //var expected = new Uri("http://localhost:60879");
+            //var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor();
+            //var handler = new MetadataEntitiesDescriptorHandler();
+            ////ACT
+            //var del = IdpMetadataHandlerFactory.GetDelegateForIdpLocation(typeof(EntitiesDescriptor));
+            //var uri = del(handler, metadata, new Uri(ProtocolBindings.HttpRedirect));
+            ////ASSERT
+            //Assert.AreEqual(expected, uri);
         }
         #endregion
     }
