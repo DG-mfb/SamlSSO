c88963f66a8f6d32add812579464227c400f3fa5
dg-mfb <danail.georgiev73@gmail.com>
2017-10-30 10:32:25 +0000

    metadata handler tests fixing

---
+++ Authorization/Federation/Federation.Metadata.Consumer.Tests/MetadataHandlerTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IdentityModel.Metadata;
+using System.Linq;
+using Federation.Metadata.Consumer.Tests.Mock;
+using Federation.Metadata.FederationPartner.Handlers;
+using NUnit.Framework;
+using Shared.Federtion.Constants;
+using Shared.Federtion.Factories;
+
+namespace Federation.Metadata.Consumer.Tests
+{
+    [TestFixture]
+    internal class MetadataHandlerTests
+    {
+        [Test]
+        public void GetDelegateForIdpDescriptors_entity_descriptor_metadata_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetIdpEntityDescriptor();
+            var handler = new MetadataEntitityDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetRoleDescriptors<IdentityProviderSingleSignOnDescriptor>(metadata)
+                .ToList();
+            //ASSERT
+            Assert.AreEqual(1, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptor_entity_descriptor_metadata_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetIdpEntityDescriptor();
+            var handler = new MetadataEntitityDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetIdentityProviderSingleSignOnDescriptor(metadata)
+                .ToList();
+            //ASSERT
+            Assert.AreEqual(1, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptors_entity_descriptor_metadata_sp_role_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetSpEntityDescriptor();
+            var handler = new MetadataEntitityDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetRoleDescriptors<ServiceProviderSingleSignOnDescriptor>(metadata)
+                .ToList();
+            //ASSERT
+            Assert.AreEqual(1, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptors_entities_descriptor_metadata_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor(1, 1);
+            var handler = new MetadataEntitiesDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetRoleDescriptors<IdentityProviderSingleSignOnDescriptor>(metadata)
+               .ToList();
+            //ASSERT
+            Assert.AreEqual(1, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptors_entities_descriptor_metadata_sp_role_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor(1, 1);
+            var handler = new MetadataEntitiesDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetRoleDescriptors<ServiceProviderSingleSignOnDescriptor>(metadata)
+               .ToList();
+            //ASSERT
+            Assert.AreEqual(1, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptors_entities_descriptor_metadata_multiple_child_entities_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor(2, 1);
+            var handler = new MetadataEntitiesDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetRoleDescriptors<IdentityProviderSingleSignOnDescriptor>(metadata)
+               .ToList();
+            //ASSERT
+            Assert.AreEqual(2, roleDescriptors.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptor_entities_descriptor_metadata_multiple_child_entities_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor(2, 1);
+            var handler = new MetadataEntitiesDescriptorHandler();
+            //ACT
+            var roleDescriptors = handler.GetIdentityProviderSingleSignOnDescriptor(metadata)
+               .ToList();
+            //ASSERT
+            Assert.AreEqual(2, roleDescriptors.Count);
+        }
+    }
+}
--- Authorization/Federation/Federation.Metadata.Consumer.Tests/Mock/EntityDescriptorProviderMock.cs
+++ Authorization/Federation/Federation.Metadata.Consumer.Tests/Mock/EntityDescriptorProviderMock.cs
@@ -7,7 +7,8 @@ namespace Federation.Metadata.Consumer.Tests.Mock
 {
     public class EntityDescriptorProviderMock
     {
-        public static EntityDescriptor GetEntityDescriptor()
+        private static X509Certificate2 mockCert;
+        public static EntityDescriptor GetIdpEntityDescriptor()
         {
             var descriptor = new EntityDescriptor();
             var cert = EntityDescriptorProviderMock.GetMockCertificate();
@@ -17,23 +18,54 @@ namespace Federation.Metadata.Consumer.Tests.Mock
             return descriptor;
         }
 
-        public static EntitiesDescriptor GetEntitiesDescriptor()
+        public static EntityDescriptor GetSpEntityDescriptor()
+        {
+            var descriptor = new EntityDescriptor();
+            var cert = EntityDescriptorProviderMock.GetMockCertificate();
+            var idpRole = new ServiceProviderSingleSignOnDescriptor();
+            idpRole.AssertionConsumerServices.Add(0, new IndexedProtocolEndpoint(0, new Uri(ProtocolBindings.HttpRedirect), new Uri("http://localhost:60879")));
+            descriptor.RoleDescriptors.Add(idpRole);
+            return descriptor;
+        }
+
+        public static EntitiesDescriptor GetEntitiesDescriptor(int idpEntities, int spEntities)
         {
             var descriptor = new EntitiesDescriptor();
-            var entityDesc = EntityDescriptorProviderMock.GetEntityDescriptor();
-            descriptor.ChildEntities.Add(entityDesc);
+            for (var i = 0; i < idpEntities; i++)
+            {
+                var entityDesc = EntityDescriptorProviderMock.GetIdpEntityDescriptor();
+                descriptor.ChildEntities.Add(entityDesc);
+            }
+
+            for (var i = 0; i < spEntities; i++)
+            {
+                var entityDesc = EntityDescriptorProviderMock.GetSpEntityDescriptor();
+                descriptor.ChildEntities.Add(entityDesc);
+            }
             return descriptor;
         }
 
         public static X509Certificate2 GetMockCertificate()
         {
-            var store = new X509Store(StoreName.My);
-            store.Open(OpenFlags.ReadOnly);
-            var certSource = store.Certificates[0];
-            var rawData = certSource.GetRawCertData();
-            var cert = new X509Certificate2(rawData);
-            store.Close();
-            return cert;
+            if (EntityDescriptorProviderMock.mockCert == null)
+            {
+                using (var store = new X509Store(StoreName.My))
+                {
+                    try
+                    {
+                        store.Open(OpenFlags.ReadOnly);
+                        var certSource = store.Certificates[0];
+                        var rawData = certSource.GetRawCertData();
+                        var cert = new X509Certificate2(rawData);
+                        EntityDescriptorProviderMock.mockCert = cert;
+                    }
+                    finally
+                    {
+                        store.Close();
+                    }
+                }
+            }
+            return EntityDescriptorProviderMock.mockCert;
         }
     }
 }
