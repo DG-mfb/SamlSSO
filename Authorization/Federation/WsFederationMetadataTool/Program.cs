c63f2340009b5c42daa5fd7b17c7f97229fb8818
dg-mfb <danail.georgiev73@gmail.com>
2017-10-30 13:09:37 +0000

    console tool to create a sp metadata instead of test

--- Authorization/Federation/WsFederationMetadataTool/Program.cs
+++ Authorization/Federation/WsFederationMetadataTool/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kernel.Federation.MetaData;
 using Kernel.Initialisation;
 using Kernel.Logging;
 using ServerInitialisation;
@@ -14,6 +16,7 @@ namespace WsFederationMetadataTool
     {
         static void Main(string[] args)
         {
+            var path = @"D:\Dan\Software\Apira\SPMetadata\SPMetadataTest0123.xml";
             using (new InformationLogEventWriter())
             {
                 ApplicationConfiguration.RegisterDependencyResolver(() => new UnityDependencyResolver());
@@ -21,6 +24,29 @@ namespace WsFederationMetadataTool
                 var task = Initialisation.Init();
                 task.Wait();
             }
+
+            var metadataTask = Program.CreateMetadata(path);
+            metadataTask.Wait();
+            Console.ReadLine();
+        }
+
+        static async Task CreateMetadata(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                var federationParty = "local";//FederationPartyIdentifierHelper.GetFederationPartyIdFromRequestOrDefault(c);
+                var metadataGenerator = Program.ResolveMetadataGenerator<ISPMetadataGenerator>();
+
+                var metadataRequest = new MetadataGenerateRequest(MetadataType.SP, federationParty, new MetadataPublishContext(fs, MetadataPublishProtocol.FileSystem));
+                await metadataGenerator.CreateMetadata(metadataRequest);
+            }
+        }
+
+        private static TMetadatGenerator ResolveMetadataGenerator<TMetadatGenerator>() where TMetadatGenerator : IMetadataGenerator
+        {
+            var resolver = ApplicationConfiguration.Instance.DependencyResolver;
+            var metadataGenerator = resolver.Resolve<TMetadatGenerator>();
+            return metadataGenerator;
         }
     }
 }
