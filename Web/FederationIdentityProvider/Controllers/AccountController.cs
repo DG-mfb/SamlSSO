52db84c9df55fa4a19e7e5cbf21d3e6b77ea270f
dg-mfb <danail.georgiev73@gmail.com>
2017-11-04 13:25:10 +0000

    account controller to idp web api

---
+++ Web/FederationIdentityProvider/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FederationIdentityProvider.Controllers
+{
+    [RoutePrefix("account")]
+    public class AccountController : ApiController
+    {
+        [Route("sso/login")]
+        [HttpGet]
+        public IHttpActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
