744dcfc7190e8733f94f14661afac5ce0f9ddbc2
dg-mfb <danail.georgiev73@gmail.com>
2017-11-06 14:59:33 +0000

    adding fake user for test

--- Web/FederationIdentityProvider/Client/app/login/login.component.ts
+++ Web/FederationIdentityProvider/Client/app/login/login.component.ts
@@ -2,6 +2,8 @@
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { AlertService, AuthenticationService } from '../_services/index';
+import { UserService } from "../_services/user.service";
+import { User } from "../_models/user";
 
 @Component({
     moduleId: module.id,
@@ -14,15 +16,21 @@ export class LoginComponent implements OnInit {
     returnUrl: string;
 
     constructor(
-        private route: ActivatedRoute,
+		private route: ActivatedRoute,
+		private userService: UserService,
         private router: Router,
         private authenticationService: AuthenticationService,
         private alertService: AlertService) { }
 
     ngOnInit() {
         // reset login status
-        this.authenticationService.logout();
-
+		this.authenticationService.logout();
+		var testUser = new User();
+		testUser.username = "d@domain.com";
+		testUser.password = "111";
+		testUser.firstName = "John";
+		testUser.lastName = "Dow";
+		this.userService.create(testUser)
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
