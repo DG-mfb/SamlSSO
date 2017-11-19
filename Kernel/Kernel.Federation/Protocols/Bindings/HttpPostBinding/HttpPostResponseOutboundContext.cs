95138a6c129da796354be9aff885608f7ff4d532
dg-mfb <danail.georgiev73@gmail.com>
2017-11-19 11:09:27 +0000

    clean up

--- Kernel/Kernel.Federation/Protocols/Bindings/HttpPostBinding/HttpPostResponseOutboundContext.cs
+++ Kernel/Kernel.Federation/Protocols/Bindings/HttpPostBinding/HttpPostResponseOutboundContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Claims;
-
-namespace Kernel.Federation.Protocols.Bindings.HttpPostBinding
+﻿namespace Kernel.Federation.Protocols.Bindings.HttpPostBinding
 {
     public class HttpPostResponseOutboundContext : SamlOutboundContext
     {
