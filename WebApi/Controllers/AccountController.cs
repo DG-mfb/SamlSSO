f72fbe1fb1bc34683158158276a3b8cebb4f4f41
dg-mfb <danail.georgiev73@gmail.com>
2017-12-02 08:59:11 +0000

    clean up

--- WebApi/Controllers/AccountController.cs
+++ WebApi/Controllers/AccountController.cs
@@ -2,6 +2,10 @@
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Linq;
+using Microsoft.Owin.Security.DataHandler;
+using Microsoft.Owin.Security.DataProtection;
+using System.Web;
+using Microsoft.Owin.Security;
 
 namespace WebApi.Controllers
 {
@@ -12,13 +16,24 @@ namespace WebApi.Controllers
         [Route("SSOLogon")]
         public async Task<IHttpActionResult> SSOLogon()
         {
+            var context = System.Web.HttpContextExtensions.GetOwinContext(HttpContext.Current.Request);
+            var samlAuthType = await context.Authentication.AuthenticateAsync("Saml2SSO");
+
+            var protector = new DpapiDataProtectionProvider("SSO server").Create("token");
+            var dataFormat = new TicketDataFormat(protector);
+           
             var identity = (ClaimsIdentity)base.RequestContext.Principal.Identity;
-            var origin = identity.FindFirst(ClaimTypes.Uri).Value;
-            var identityClaim = identity.Claims.Where(c => c.Value.Contains("@"))
-                .FirstOrDefault(c => c.Subject.NameClaimType == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
-            if(identityClaim != null)
-                return Ok(identityClaim);
-            return Ok(identity.Claims);
+            var ticket = new AuthenticationTicket(identity, samlAuthType.Properties);
+            var serialisedTicket = dataFormat.Protect(ticket);
+            var targetUri = string.Format("http://localhost:61463/api/SSO?token={0}", serialisedTicket);
+            return Redirect(targetUri);
+            //return Ok(serialisedTicket);
+            //var origin = identity.FindFirst(ClaimTypes.Uri).Value;
+            //var identityClaim = identity.Claims.Where(c => c.Value.Contains("@"))
+            //    .FirstOrDefault(c => c.Subject.NameClaimType == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
+            //if(identityClaim != null)
+            //    return Ok(identityClaim);
+            //return Ok(identity.Claims);
         }
     }
 }
