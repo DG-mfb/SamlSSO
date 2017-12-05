227b0eec4262fd41fbcdc573999eaa8f6756bdc7
dg-mfb <danail.georgiev73@gmail.com>
2017-12-05 12:12:24 +0000

    clean up

--- WebApi/Token/SSOAuthorizationServerProvider.cs
+++ WebApi/Token/SSOAuthorizationServerProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ using Kernel.DependancyResolver;
 using Kernel.Federation.Authorization;
 using Kernel.Federation.FederationPartner;
 using Shared.Federtion.Constants;
+using SSOOwinMiddleware.Contexts;
 
 namespace WebApi.Token
 {
@@ -20,20 +22,28 @@ namespace WebApi.Token
         async Task IAuthorizationServerProvider.TokenEndpointResponse<TContext>(TContext context)
         {
             var relayState = context.RelayState;
+            if (!relayState.ContainsKey(RelayStateContstants.FederationPartyId))
+                throw new InvalidOperationException("Federation party id is not in the relay state.");
+
             var federationPartyId = relayState[RelayStateContstants.FederationPartyId].ToString();
             var configurationManager = this._resolver.Resolve<IConfigurationManager<AuthorizationServerConfiguration>>();
             var configuration = await configurationManager.GetConfigurationAsync(federationPartyId, CancellationToken.None);
+            //if no configuration for the parner return, no need to throw an exception.
             if (configuration == null)
                 return;
-            
-            //if (!relayState.ContainsKey("returnUrl"))
-            //    return;
-            //var returnUrl = relayState["returnUrl"].ToString();
-            var httpClient = new HttpClient();
-            
-            var result = await HttpClientExtensions.PostAsJsonAsync(httpClient, configuration.TokenResponseUrl, context.Token);
-            
-            context.RequestCompleted();
+            //cast the context as a SSOTokenEndpointResponseContext to access IOwinContext if you want to wrire to the
+            //response. it the request is get for example
+            var sSOTokenEndpointResponseContext = context as SSOTokenEndpointResponseContext;
+            if (sSOTokenEndpointResponseContext != null)
+            {
+                var httpClient = new HttpClient();
+                //for the demo pusposes only. more likely it will be a get request so we need to return the token by writting to the 
+                //response.
+                //for instance 
+                var result = await HttpClientExtensions.PostAsJsonAsync(httpClient, configuration.TokenResponseUrl, context.Token);
+
+                context.RequestCompleted();
+            }
         }
     }
 }
