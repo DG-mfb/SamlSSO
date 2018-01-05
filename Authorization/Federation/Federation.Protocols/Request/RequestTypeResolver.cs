1a88539d077513de6aa8bb39a8cfd90b84d3fd90
dg-mfb <dgmfb73@gmail.com>
2018-01-05 09:37:09 +0000

    clean up

--- Authorization/Federation/Federation.Protocols/Request/RequestTypeResolver.cs
+++ Authorization/Federation/Federation.Protocols/Request/RequestTypeResolver.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using Kernel.Federation.Protocols;
 using Kernel.Reflection;
-using Kernel.Reflection.Extensions;
 using Shared.Federtion.Constants;
 using Shared.Federtion.Models;
 
