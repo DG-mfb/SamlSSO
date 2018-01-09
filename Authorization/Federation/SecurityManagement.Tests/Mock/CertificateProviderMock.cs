e263ba80c9781804825a02c015b72b4449909d9f
dg-mfb <dgmfb73@gmail.com>
2018-01-09 15:03:39 +0000

    xml signature tests

---
+++ Authorization/Federation/SecurityManagement.Tests/Mock/CertificateProviderMock.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SecurityManagement.Tests.Mock
+{
+    internal class CertificateProviderMock
+    {
+        private static X509Certificate2 mockCert;
+
+        public static X509Certificate2 GetMockCertificate()
+        {
+            if (CertificateProviderMock.mockCert == null)
+            {
+                using (var store = new X509Store("testCertStore", StoreLocation.LocalMachine))
+                {
+                    try
+                    {
+                        store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                        var certSource = store.Certificates.Find(X509FindType.FindBySubjectName, "www.eca-international.com", false)[0];
+                        CertificateProviderMock.mockCert = certSource;
+                    }
+                    finally
+                    {
+                        store.Close();
+                    }
+                }
+            }
+            return CertificateProviderMock.mockCert;
+        }
+    }
+}
--- Authorization/Federation/SecurityManagement.Tests/Signing/XmlSignatureManagerTest.cs
+++ Authorization/Federation/SecurityManagement.Tests/Signing/XmlSignatureManagerTest.cs
@@ -1,14 +1,117 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using NUnit.Framework;
+using SecurityManagement.Signing;
+using System.Security.Cryptography.X509Certificates;
+using Kernel.Federation.Constants;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Selectors;
+using SecurityManagement.Tests.Mock;
 
 namespace SecurityManagement.Tests.Signing
 {
     [TestFixture]
     internal class XmlSignatureManagerTest
     {
+        [Test]
+        [Ignore("Temp")]
+        public void TempTest()
+        {
+            //ARRANGE
+            var signatureManager = new XmlSignatureManager();
+            var document = new XmlDocument();
+            document.Load(@"D:\Dan\Software\ECA-Interenational\Temp\AtlasResponse.xml");
+            var signEl = document.GetElementsByTagName("Signature", "http://www.w3.org/2000/09/xmldsig#")
+                .Cast<XmlElement>()
+                .First(x => x.ParentNode == document.DocumentElement);
+            var certEl = (XmlElement)signEl.GetElementsByTagName("X509Certificate", "http://www.w3.org/2000/09/xmldsig#")[0];
+            var dcert2 = new X509Certificate2(Convert.FromBase64String(certEl.InnerText));
+
+            //ACT
+            var isValid = signatureManager.VerifySignature(document, signEl, dcert2.PublicKey.Key);
+            //ASSERT
+            Assert.True(isValid);
+        }
+
+        [Test]
+        [Ignore("Temp test")]
+        public void TempAssertionTest()
+        {
+            //ARRANGE
+            var signatureManager = new XmlSignatureManager();
+            var document = new XmlDocument();
+            document.Load(@"D:\Dan\Software\ECA-Interenational\Temp\AtlasResponse.xml");
+            var assertion = (XmlElement)document.GetElementsByTagName("Assertion", Saml20Constants.Assertion)[0];
+            var signEl = document.GetElementsByTagName("Signature", "http://www.w3.org/2000/09/xmldsig#")
+                .Cast<XmlElement>()
+                .First(x => x.ParentNode == assertion);
+            var certEl = (XmlElement)signEl.GetElementsByTagName("X509Certificate", "http://www.w3.org/2000/09/xmldsig#")[0];
+            var dcert2 = new X509Certificate2(Convert.FromBase64String(certEl.InnerText));
+            var document1 = new XmlDocument();
+            document1.LoadXml(assertion.OuterXml);
+            var handler = new Saml2SecurityTokenHandler();
+            var config = this.GetConfiguration();
+            handler.Configuration = config;
+            var reader = XmlReader.Create(new StringReader(document.OuterXml));
+            this.MoveToToken(reader);
+            var reader1 = XmlReader.Create(new StringReader(document1.OuterXml));
+            var token = handler.ReadToken(reader);
+            var token1 = handler.ReadToken(reader1);
+            //ACT
+            var isValid = signatureManager.VerifySignature(document1, signEl, dcert2.PublicKey.Key);
+            var isValid1 = signatureManager.VerifySignature(document, signEl, dcert2.PublicKey.Key);
+            //ASSERT
+            Assert.True(isValid);
+        }
+
+        [Test]
+        public void SignRequest()
+        {
+            //ARRANGE
+            var signatureManager = new XmlSignatureManager();
+            var document = new XmlDocument();
+            document.Load(@"D:\Dan\Software\ECA-Interenational\Temp\AuthnRequest.xml");
+            var cert = CertificateProviderMock.GetMockCertificate();
+
+            //ACT
+            signatureManager.SignXml(document, "eca_e2376d50-2f84-4a6b-bdc6-8283a8b2d990_bde559c1-fe68-4ff8-898f-5660a794156a", cert.PrivateKey);
+
+            var signEl = document.GetElementsByTagName("Signature", "http://www.w3.org/2000/09/xmldsig#")
+                .Cast<XmlElement>()
+                .First(x => x.ParentNode == document.DocumentElement);
+            var isValid = signatureManager.VerifySignature(document, signEl, cert.PublicKey.Key);
+            //ASSERT
+            Assert.True(isValid);
+        }
+
+        public SecurityTokenHandlerConfiguration GetConfiguration()
+        {
+            var inner = new X509CertificateStoreTokenResolver("testCertStore", StoreLocation.LocalMachine);
+            var tokenResolver = new IssuerTokenResolver(inner);
+            var configuration = new SecurityTokenHandlerConfiguration
+            {
+                IssuerTokenResolver = tokenResolver,
+                ServiceTokenResolver = inner,
+                AudienceRestriction = new AudienceRestriction(AudienceUriMode.Always),
+                CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.Custom,
+            };
+            
+            return configuration;
+        }
+
+        internal void MoveToToken(XmlReader reader)
+        {
+            while (!(reader.IsStartElement(HttpRedirectBindingConstants.EncryptedAssertion, Saml20Constants.Assertion) || reader.IsStartElement("Assertion", Saml20Constants.Assertion)))
+            {
+                if (!reader.Read())
+                    throw new InvalidOperationException("Can't find assertion element.");
+            }
+        }
     }
 }
