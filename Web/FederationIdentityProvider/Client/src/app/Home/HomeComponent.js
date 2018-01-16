c042d4378355d59892fdbbdc7df8feaeacb479cc
dg-mfb <dgmfb73@gmail.com>
2018-01-16 07:10:31 +0000

    idp js

--- Web/FederationIdentityProvider/Client/src/app/Home/HomeComponent.js
+++ Web/FederationIdentityProvider/Client/src/app/Home/HomeComponent.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 //import { User } from '../_models/index';
 //import { UserService } from '../_services/index';
-var HomeComponent = /** @class */ (function () {
+var HomeComponent = (function () {
     //currentUser: User;
     //users: User[] = [];
     function HomeComponent() {
@@ -23,14 +23,14 @@ var HomeComponent = /** @class */ (function () {
     HomeComponent.prototype.loadAllUsers = function () {
         //this.userService.getAll().subscribe(users => { this.users = users; });
     };
-    HomeComponent = __decorate([
-        core_1.Component({
-            moduleId: module.id,
-            templateUrl: 'home.component.html'
-        }),
-        __metadata("design:paramtypes", [])
-    ], HomeComponent);
     return HomeComponent;
 }());
+HomeComponent = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        templateUrl: 'home.component.html'
+    }),
+    __metadata("design:paramtypes", [])
+], HomeComponent);
 exports.HomeComponent = HomeComponent;
 //# sourceMappingURL=HomeComponent.js.map
