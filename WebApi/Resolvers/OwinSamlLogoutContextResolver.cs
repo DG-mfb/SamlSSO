f250668605b2b33aec59b2724348980e87203ef3
dg-mfb <dgmfb73@gmail.com>
2018-01-16 07:26:40 +0000

    log out resolver context set up

---
+++ WebApi/Resolvers/OwinSamlLogoutContextResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Kernel.DependancyResolver;
+using Kernel.Federation.Protocols;
+using Microsoft.Owin;
+
+namespace WebApi.Resolvers
+{
+    internal class OwinSamlLogoutContextResolver : ISamlLogoutContextResolver<IOwinRequest>
+    {
+        private readonly IDependencyResolver _resolver;
+
+        public OwinSamlLogoutContextResolver(IDependencyResolver resolver)
+        {
+            this._resolver = resolver;
+        }
+        public SamlLogoutContext ResolveLogoutContext(IOwinRequest request)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
