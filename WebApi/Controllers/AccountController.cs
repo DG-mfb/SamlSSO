5b8b9e6c03ab0cd9edcd115ba29c9c653d43d139
Daniel Georgiev <danielg@eca-international.com>
2018-01-19 09:12:11 +0000

    adding some helper classes to help particular case tests

--- WebApi/Controllers/AccountController.cs
+++ WebApi/Controllers/AccountController.cs
@@ -1,12 +1,11 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Linq;
-using Microsoft.Owin.Security.DataHandler;
-using Microsoft.Owin.Security.DataProtection;
-using System.Web;
+﻿using Kernel.Initialisation;
 using Microsoft.Owin.Security;
 using System;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.Results;
 
 namespace WebApi.Controllers
 {
@@ -17,14 +16,28 @@ namespace WebApi.Controllers
         [Route("SSOLogon")]
         public async Task<IHttpActionResult> SSOLogon()
         {
-            throw new NotImplementedException();
+            var resolver = ApplicationConfiguration.Instance.DependencyResolver;
+            var protector = resolver.Resolve<ISecureDataFormat<AuthenticationTicket>>();
+            
+            var identity = (ClaimsIdentity)User.Identity;
+            var ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+            var token = protector.Protect(ticket);
+            var redirectUrl = String.Format("http://localhost:49974/Logout.aspx?Bearer={0}", token);
+            //return Redirect(String.Format("http://localhost:49974/Logout.aspx?Bearer={0}", token));
+            return new CookieRedirectResult(new Uri(redirectUrl), new HttpRequestMessage(Request.Method, Request.RequestUri.AbsoluteUri), (ClaimsIdentity)User.Identity);
+            
+            //FormsAuthentication.SetAuthCookie("testUser", false);
+            //HttpContext.Current.Response.Headers.Add("Authorization", "Bearer " + token);
+            //HttpContext.Current.Response.Cookies.Add(new HttpCookie("Bearer", token));
+            //return Redirect(new Uri("http://localhost:49974/Logout.aspx"));
+            //throw new NotImplementedException();
             //return Ok();
             //var context = System.Web.HttpContextExtensions.GetOwinContext(HttpContext.Current.Request);
             //var samlAuthType = await context.Authentication.AuthenticateAsync("Saml2SSO");
 
             //var protector = new DpapiDataProtectionProvider("SSO server").Create("token");
             //var dataFormat = new TicketDataFormat(protector);
-           
+
             //var identity = (ClaimsIdentity)base.RequestContext.Principal.Identity;
             //var ticket = new AuthenticationTicket(identity, samlAuthType.Properties);
             //var serialisedTicket = dataFormat.Protect(ticket);
@@ -39,14 +52,12 @@ namespace WebApi.Controllers
             //return Ok(identity.Claims);
         }
 
-        [Route("Logout")]
-        [HttpGet]
+        [Route("SSOLogout")]
+        [HttpPost]
         [AllowAnonymous]
-        public async Task<IHttpActionResult> Logout()
+        public async Task<IHttpActionResult> SSOLogout()
         {
-            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
-            authenticationManager.SignOut(new AuthenticationProperties(), "Saml2SLO");
-            return Ok();
+            return Ok("Logged out.");
         }
     }
 }
---
+++ WebApi/Results/CookieRedirectResult.cs
@@ -0,0 +1,76 @@
+﻿using Kernel.Initialisation;
+using Microsoft.Owin.Security;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.Results
+{
+    public class CookieRedirectResult : IHttpActionResult
+    {
+        private readonly Uri _location;
+        private readonly HttpRequestMessage _request;
+        private ClaimsIdentity _identity;
+
+        public CookieRedirectResult(Uri location, HttpRequestMessage request, ClaimsIdentity identity)
+        {
+            if (location == (Uri)null)
+                throw new ArgumentNullException(nameof(location));
+            this._location = location;
+            this._request = request;
+            this._identity = identity;
+        }
+
+        public Uri Location
+        {
+            get
+            {
+                return this._location;
+            }
+        }
+
+        public HttpRequestMessage Request
+        {
+            get
+            {
+                return this._request;
+            }
+        }
+
+        public virtual Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult<HttpResponseMessage>(this.Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.Found);
+            try
+            {
+                var resolver = ApplicationConfiguration.Instance.DependencyResolver;
+                var protector = resolver.Resolve<ISecureDataFormat<AuthenticationTicket>>();
+
+                var ticket = new AuthenticationTicket(this._identity, new AuthenticationProperties());
+                var token = protector.Protect(ticket);
+                var cookie = new CookieHeaderValue("Bearer", token);
+                cookie.Expires = DateTimeOffset.Now.AddDays(1);
+                cookie.Domain = Request.RequestUri.Host;
+                cookie.Path = "/";
+                httpResponseMessage.Headers.AddCookies(new[] { cookie });
+                httpResponseMessage.Headers.Location = this._location;
+                httpResponseMessage.RequestMessage = this._request;
+            }
+            catch
+            {
+                httpResponseMessage.Dispose();
+                throw;
+            }
+            return httpResponseMessage;
+        }
+    }
+}
