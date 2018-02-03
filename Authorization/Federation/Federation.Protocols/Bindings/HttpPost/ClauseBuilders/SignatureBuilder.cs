c5c5773e77f4bbd736a0f0ae405d5878593a1b89
Daniel Georgiev <danielg@eca-international.com>
2018-02-03 20:19:44 +0000

    post binding signature changes

--- Authorization/Federation/Federation.Protocols/Bindings/HttpPost/ClauseBuilders/SignatureBuilder.cs
+++ Authorization/Federation/Federation.Protocols/Bindings/HttpPost/ClauseBuilders/SignatureBuilder.cs
@@ -44,6 +44,10 @@ namespace Federation.Protocols.Bindings.HttpPost.ClauseBuilders
                 var document = new XmlDocument();
                 document.LoadXml(request);
                 this._xmlSignatureManager.WriteSignature(document, requestContext.RequestId, cert.PrivateKey, "", "");
+                var signature = document.GetElementsByTagName("Signature")[0];
+                signature.ParentNode.RemoveChild(signature);
+                var issuer = document.GetElementsByTagName("saml:Issuer")[0];
+                issuer.ParentNode.InsertAfter(signature, issuer);
                 this._logProvider.LogMessage(String.Format("Authentication request signed./r/n{0}", document.OuterXml));
                 context.RequestParts[HttpRedirectBindingConstants.SamlRequest] = document.OuterXml;
             }
