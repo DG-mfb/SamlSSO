b90615294e539695b03903bd6d24db0af87cbc82
dg-mfb <dgmfb73@gmail.com>
2018-02-04 12:49:35 +0000

    signature builder refactoring

--- Authorization/Federation/Federation.Protocols/Bindings/HttpPost/ClauseBuilders/SignatureBuilder.cs
+++ Authorization/Federation/Federation.Protocols/Bindings/HttpPost/ClauseBuilders/SignatureBuilder.cs
@@ -46,9 +46,12 @@ namespace Federation.Protocols.Bindings.HttpPost.ClauseBuilders
                 document.LoadXml(request);
                 this._xmlSignatureManager.WriteSignature(document, requestContext.RequestId, cert.PrivateKey, "", "");
                 var signature = TokenHelper.GetElement("Signature", "http://www.w3.org/2000/09/xmldsig#", document.DocumentElement);
+                if (signature == null)
+                    throw new InvalidOperationException("Signature element not present.");
                 signature.ParentNode.RemoveChild(signature);
                 var issuer = TokenHelper.GetElement("Issuer", Saml20Constants.Assertion, document.DocumentElement);
-                
+                if (issuer == null)
+                    throw new InvalidOperationException("Issuer element not present.");
                 issuer.ParentNode.InsertAfter(signature, issuer);
                 this._logProvider.LogMessage(String.Format("Authentication request signed./r/n{0}", document.OuterXml));
                 context.RequestParts[HttpRedirectBindingConstants.SamlRequest] = document.OuterXml;
