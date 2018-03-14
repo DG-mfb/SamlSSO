7ef592b921185ee7e001e85308be700b28fd6bb7
Daniel Georgiev <danielg@eca-international.com>
2018-03-14 09:09:55 +0000

    identity service user management methods added

--- AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/IdentityProviderService.cs
+++ AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/IdentityProviderService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AspNet.EntityFramework.IdentityProvider.Managers;
@@ -30,6 +31,42 @@ namespace AspNet.EntityFramework.IdentityProvider
                 identity = await this._claimsProvider.GenerateUserIdentitiesAsync(user, context.AuthenticationTypes);
             }
             return new AuthenticationResult(authenticationResult, identity);
+        }
+        public async Task<UserInfoResult> FindUserByEmail(string email)
+        {
+            var user = await this._manager.FindByEmailAsync(email);
+            if (user == null)
+                return null;
+            return new UserInfoResult(user.Id, user.Email);
+        }
+
+        public async Task<string> GeneratePasswordResetToken(string userId)
+        {
+            return await this._manager.GeneratePasswordResetTokenAsync(userId);
+        }
+
+        public async Task<bool> ResetPasswordAsync(string userId, string token, string newPassword)
+        {
+            try
+            {
+                var user = await this._manager.FindByIdAsync(userId);
+                var result = await this._manager.ResetPasswordAsync(userId, token, newPassword);
+                return result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> ValidateResetPasswordTokenAsync(string userId, string token)
+        {
+            var protector = this._manager.UserTokenProvider;
+            var user = await this._manager.FindByIdAsync(userId);
+            if (user == null)
+                return false;
+            var isValid = await protector.ValidateAsync("ResetPassword", token, this._manager, user);
+            return isValid;
         }
     }
 }
